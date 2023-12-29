e42fb0ed6529d045f294ae1e39d1466b64b586e2
shinthanthtun00 <shinthanthtun00@gmail.com>
2023-12-29 20:02:45 +0630

    Add useEffect hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 import {Splide, SplideSlide} from "@splidejs/react-splide"
 import '@splidejs/react-splide/css';
 import HeaderCard from "./HeaderCard";
+import { useEffect } from "react";
 
 function Header() {
+    useEffect(() => {
+      getRelatedMovies();
+  },[]);
+
+  const getRelatedMovies = async () => {
+    const response = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=ae1aef421d4eefc14e88dc9097be2e49&language=en-US");
+    
+    const data = await response.json()
+    console.log(data);
+  }
+
+
+
   return (
     <section>
       <Splide options={{
@@ -11,11 +25,6 @@ function Header() {
         <SplideSlide>
           <HeaderCard imgSrc={"https://images4.alphacoders.com/132/1323605.jpeg"} title={"Oppenheimer"}/>
         </SplideSlide>
-
-        <SplideSlide>
-          <HeaderCard imgSrc={"https://images7.alphacoders.com/550/550739.jpg"} title={"Interstellar"}/>
-        </SplideSlide>
-        
       </Splide>
     </section>
   )
